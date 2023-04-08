fe1276ee7cd87cb7ecf08086fe8b5f3fe9844345
Jakub Wroński <wronski2002@wp.pl>
2023-04-08 15:46:52 +0200

    Project setup

---
+++ resources/ts/Pages/Main.tsx
@@ -0,0 +1,16 @@
+// import React from "react";
+
+// interface WelcomeProps {}
+
+// const Main: React.FC<WelcomeProps> = (props) => {
+//     return <>Welcome from TSX</>;
+// };
+// export default Main;
+
+import React from "react";
+
+const Main = (): JSX.Element => {
+    return <div>Main</div>;
+};
+
+export default Main;
